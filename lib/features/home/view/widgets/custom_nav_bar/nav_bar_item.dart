63a34696cba4a703799b047671455b54fcc9a86f
saeedahmed725 <98642572+saeedahmed725@users.noreply.github.com>
2023-11-04 17:42:36 +0200

    init

--- lib/features/home/view/widgets/custom_nav_bar/nav_bar_item.dart
+++ lib/features/home/view/widgets/custom_nav_bar/nav_bar_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:svg_flutter/svg.dart';
+import 'package:task/core/constants/app_colors.dart';
 
 import '../../../../../core/constants/static.dart';
 import '../../../../../core/controller/home_controller.dart';
@@ -23,7 +24,10 @@ class NavBarItem extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SvgPicture.asset(Static.navBarImage[index]),
+            Obx(() => SvgPicture.asset(Static.navBarImage[index],
+                color: homeController.currentIndexNavBar.value == index
+                    ? AppColor.kPrimaryColor
+                    : AppColor.kBrightGrayColor)),
             CustomText(text: Static.appNavBarTitles[index])
           ],
         ),
